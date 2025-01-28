eb8cdb229afcd7e44148722d8fb27434a3fab6e7
Shivam Malhotra <malhotrashivam77@gmail.com>
2025-01-28 23:24:51 +0530

    update

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/service')
+def service():
+    return render_template('service.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
