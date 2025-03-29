7cdc4dc42bcf3d5c4d4f379799493f0d4710a7a1
Shivam Malhotra <malhotrashivam77@gmail.com>
2025-03-29 13:44:38 +0530

    update

--- app.py
+++ app.py
@@ -11,5 +11,25 @@ def index():
 def service():
     return render_template('service.html')
 
+@app.route('/architecture')
+def architecture():
+    return render_template('architecture.html')
+
+@app.route('/interior')
+def interior():
+    return render_template('interior.html')
+
+@app.route('/construction')
+def construction():
+    return render_template('construction.html')
+
+@app.route('/renovation')
+def renovation():
+    return render_template('renovation.html')
+
+@app.route('/landscape')
+def landscape():
+    return render_template('landscape.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
